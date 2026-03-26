e6de5989549a8b1d9ad4aadf888b56c34d0ec892
arslannaseer346 <arslannaseer346@gmail.com>
2026-03-26 23:45:40 +0500

    ui updated

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
-import add from './new.js'
+import Add from './new.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <Add />
+  
 )
--- src/new.jsx
+++ src/new.jsx
@@ -3,3 +3,4 @@ function add(){
         <h2>hello this is my first react application sent</h2>
     )
 }
+export default add
